0494b1a8219be93a2c366e5854b2a7300c953b83
Richard Griffiths <griff182uk@yahoo.co.uk>
2016-02-06 11:50:47 +0000

    This is a commit

---
+++ TestingSSDTDWDeployment/Tables/DimDateExtendedProperties.sql
@@ -0,0 +1,3 @@
+﻿EXEC sp_addextendedproperty @name = N'Description', @value = N'This is a date field'
+, @level0type  = N'Schema', @level0name = N'import', @level1type = N'Table', @level1name = N'DimDate', @level2type=N'Column',@level2name = N'Date';
+GO
