a3e8012f70269cf8a9cd7881d747c0a290f6b0c2
Richard Griffiths <griff182uk@yahoo.co.uk>
2016-02-06 16:30:25 +0000

    changed calendar year to calendar year1

--- TestingSSDTDWDeployment/Tables/DimDate.sql
+++ TestingSSDTDWDeployment/Tables/DimDate.sql
@@ -2,7 +2,7 @@
 (
 	[DateKey] INT NOT NULL PRIMARY KEY, 
     [Date] DATE NOT NULL, 
-    [CalendarYear] SMALLINT NOT NULL, 
+    [CalendarYear1] SMALLINT NOT NULL, 
     [FinancialYear] SMALLINT NOT NULL DEFAULT -1
 );
 GO
