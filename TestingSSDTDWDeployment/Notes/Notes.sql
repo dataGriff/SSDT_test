2086d94585b965d4fa43a88976d5deb3f6747e83
Richard Griffiths <griff182uk@yahoo.co.uk>
2016-02-06 11:52:34 +0000

    inital commit

---
+++ TestingSSDTDWDeployment/Notes/Notes.sql
@@ -0,0 +1,12 @@
+﻿/*
+Create separate file for extended properties
+When amend automatically drops and recreates on publish
+
+Publishing doesnt drop table if removed from solution file
+Unless in deployment option select "Drop objects in target but not in source" in advanced publish options
+
+Don't forget I want triggers to fire so need select this option on in publishing
+
+If add column with NOT NULL will fail deployment as will be empty and violate constraint
+Add a default constraint (which will do anyway) and all is well...
+*/
---
+++ TestingSSDTDWDeployment/Tables/DimDate.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [import].[DimDate]
+(
+	[DateKey] INT NOT NULL PRIMARY KEY, 
+    [Date] DATE NOT NULL, 
+    [CalendarYear] SMALLINT NOT NULL, 
+    [FinancialYear] SMALLINT NOT NULL DEFAULT -1
+);
+GO
+
