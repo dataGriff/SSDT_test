24644b3c4e28972bb067101cd7936d4699b07cff
Richard Griffiths <griff182uk@yahoo.co.uk>
2016-02-21 15:39:30 +0000

    update job proc

--- TestingSSDTDWDeployment/Reference Master Scripts which break originally/ReturnSysJobsFromMSDB.sql
+++ TestingSSDTDWDeployment/Reference Master Scripts which break originally/ReturnSysJobsFromMSDB.sql
@@ -3,5 +3,8 @@
 AS
 --SELECT * from msdb.dbo.sysjobs j;
 
-	SELECT j.job_id, j.name FROM msdb.dbo.sysjobs j
-	where 1 != 0;
+	SELECT [job_id], [originating_server_id], [name], [enabled], [description], [start_step_id], [category_id]
+	, [owner_sid], [notify_level_eventlog], [notify_level_email], [notify_level_netsend], [notify_level_page]
+	, [notify_email_operator_id], [notify_netsend_operator_id], [notify_page_operator_id], [delete_level]
+	, [date_created], [date_modified], [version_number] FROM msdb.dbo.sysjobs j
+
